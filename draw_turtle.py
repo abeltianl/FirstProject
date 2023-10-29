ca5f3ced419aa8a4e8d730d463c4615a20774293
abeltianl <abeltianleader@gmail.com>
2023-10-29 08:40:19 +0100

    Initial commit

---
+++ draw_turtle.py
@@ -0,0 +1,14 @@
+import turtle
+
+t = turtle.Turtle()
+s = turtle.getscreen()
+
+t.forward(100)
+t.right(90)
+t.forward(90)
+t.right(90)
+t.forward(100)
+
+t.goto(50,50)
+t.goto(0,100)
+
---
+++ one.py
@@ -0,0 +1,6 @@
+
+num = 5
+
+num += 5
+num **= 2
+print(num)
